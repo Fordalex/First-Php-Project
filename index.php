53b236482ea0f26b3508d6f05185488c4e430101
Alex <alex96ford19@gmail.com>
2020-10-28 23:21:04 +0000

    Added navigation, head and footer files to include in the main project.

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Hello world!"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once "head.php"; ?>
+<body>
+    <?php include_once "navigation.php"; ?>
+
+
+
+    <p>Main content</p>
+
+
+    <?php include_once "footer.php"; ?>
+</body>
+</html>
