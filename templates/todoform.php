f64c1f4520b5d02355e5ad8154c58b814dbb13a9
Alex <alex96ford19@gmail.com>
2020-10-29 16:13:16 +0000

    Added a remove button for the todo list.

--- templates/todoform.php
+++ templates/todoform.php
@@ -5,9 +5,11 @@ include_once 'connect_mysql.php';
 // Get the data from the form
 
 if (isset($_GET['oper'])) {
-    $oper = 'toggle';
+    $oper = $_GET['oper'];
     $id = $_GET['id'];
-    $completed = $_GET['completed'];
+    if (isset($_GET['completed'])) {
+        $completed = $_GET['completed'];
+    }
 } else {
     $todo = $_POST['todo'];
     $completed = $_POST['completed'];
@@ -17,20 +19,22 @@ if (isset($_GET['oper'])) {
         $completed = 0;
     };
 };
-
+echo $id . $oper;
 
 // Create, update and delete data from the database.
 if ($oper == 'toggle') {
-    echo $id . $oper;
+   
     if ($completed) {
         $completed = 0;
     } else {
         $completed = 1;
     };
     mysqli_query($conn, "UPDATE `to_do` SET `completed`=$completed WHERE id=$id") or die('There was a problem updaing the data!');
+} elseif ($oper == 'remove') {
+    mysqli_query($conn, "DELETE FROM `to_do` WHERE id=$id");
 } else {
     mysqli_query($conn, "INSERT INTO to_do (`name`, `completed`) VALUES ('$todo',$completed)") or die('There was a problem submitting the form!');
-}
+};
 
 
 // Redriect the user back to the todo page
--- todo.php
+++ todo.php
@@ -4,21 +4,21 @@ include_once "templates/head.html";
 include_once "templates/navigation.html";
 include_once "templates/connect_mysql.php";
 
-echo "<h3>You Do To List</h3>";
+echo "<h3>Your Do To List</h3>";
 
 $result = mysqli_query($conn, "SELECT * FROM to_do");
 ?>
 <div class="justify-content-center row m-0 p-0">
     <div class="col-4 m-0 p-0">
     <?php
-    if ($result) {
         echo "<table class='todo-table'>
         <tr>
             <th>To do</th>
             <th>Completed</th>
             <th>Toggle</th>
+            <th>Remove</th>
         </tr>
-";
+        ";
 
         // each row from the todo database
         while ($row = mysqli_fetch_assoc($result)) {
@@ -41,28 +41,29 @@ $result = mysqli_query($conn, "SELECT * FROM to_do");
                             <img src='https://img.icons8.com/fluent/20/000000/change.png'/>
                         </a>
                     </td>
+                    <td class='text-center'>
+                        <a href='templates/todoform.php?oper=remove&id=$id'>
+                            <img src='https://img.icons8.com/fluent/20/000000/delete-forever.png'/>
+                        </a>
+                    </td>
                 </tr>";
         }
         echo "</table>";
 
         echo "Seach result: " . mysqli_num_rows($result) . "<br>";
        
-        } else {
-            echo "<p>There is nothing to do!</p>";
-        }
         ?>
     </div>
-</div>
+
 
 <!-- Adding a todo to the database -->
-<div class="justify-content-center row m-0 p-0 mt-4">
     <div class="col-4">
         <form action="templates/todoform.php" method="POST">
             <label>To Do</label>
-            <input class="form-control" type="text" name="todo">
+            <input class="form-control mb-3" type="text" name="todo">
             <label>Completed</label>
             <input type="checkbox" name="completed">
-            <button type="submit" class="btn btn-dark">Submit</button>
+            <button type="submit" class="btn btn-dark float-right">Submit</button>
         </form>
     </div>
 </div>
