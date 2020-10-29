7a6023340113152d32a3c24dccd5631dd0635016
Alex <alex96ford19@gmail.com>
2020-10-29 00:02:18 +0000

    Added a functions file to pass some basic information from one page to another and return a value.

--- index.php
+++ index.php
@@ -4,22 +4,34 @@
 <?php include_once "head.php"; ?>
 
 <body>
-    
+
     <!-- navigation -->
     <?php include_once "navigation.php"; ?>
 
     <!-- main content -->
     <div class="page-container">
         <?php
-            $loopCount = 5;
-            for ($i = 1; $i < $loopCount + 1; $i++) {
-                echo "<p>Hello  $i</p>";
-            }
+        $loopCount = 5;
+        for ($i = 1; $i < $loopCount + 1; $i++) {
+            echo "<p>Hello  $i</p>";
+        }
         ?>
+        <form action="functions.php" method="GET">
+            <label>Number One</label>
+            <input type="number" name="numOne" class="form-control">
+            <select name="oper">
+                <option value="add">Add</option>
+                <option value="sub">Subtract</option>
+            </select>
+            <label>Number Two</label>
+            <input type="number" name="numTwo" class="form-control">
+            <button type="submit">Submit</button>
+        </form>
     </div>
-  
+
     <!-- footer -->
     <?php include_once "footer.php"; ?>
 
 </body>
+
 </html>
